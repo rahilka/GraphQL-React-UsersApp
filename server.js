99792739f4bc7972dcf29d3a1d99dccc5fddf37a
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-07-25 14:07:30 +0200

    Added graphql route to our app

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');	//this library is like a glue, compatibility layer between GraphQL and Express
 
 const app = express();
 
+// Now, we are going to tell our express app, that any request that comes into our app,
+// looking for the route '/graphql', we want the graphql library to handle it.
+app.use('/graphql', expressGraphQL({
+	graphiql: true	//'graphiql' = Graphical - development tool
+}));
+
 app.listen(4000, () => {
 	console.log('Listening on port 4000');
-});
+}); 
